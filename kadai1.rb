917c7943d4d1af7f7f8ac5728d676181c81c6bb2
ko-kimura <kimurakousuke0122@gmail.com>
2019-04-05 20:38:50 +0900

    make fizzbuzz

--- kadai1.rb
+++ kadai1.rb
@@ -1,12 +1,26 @@
 for num in 1..100 do
-  find = num % 3
-  if   find == 0 then
+   if num % 3 == 0 && num % 5 == 0
+    puts "東京と北海道"
+  elsif num % 3 == 0
     puts "北海道"
-  # elsif num % 3  then
-  #   puts "北海道"
-  # elsif num % 5 then
-  #   puts "東京"
-   else
-     puts num
+  elsif num % 5 == 0
+    puts "東京"
+  else
+    puts num
   end
 end
+
+
+# for num in 1..100 do
+#   if num % 3 == 0
+#     puts "北海道"
+#   elsif num % 5 == 0
+#     puts "東京"
+#   else
+#     puts num
+#   end
+#
+#   if num % 3 == 0 && num % 5 == 0
+#     puts "東京と北海道"
+#   end
+# end
