0bbd0b4fd795111ad8579bdccc41d8ccd3522a04
ko-kimura <kimurakousuke0122@gmail.com>
2019-04-05 19:50:22 +0900

    first commit

---
+++ kadai1.rb
@@ -0,0 +1,12 @@
+for num in 1..100 do
+  find = num % 3
+  if   find == 0 then
+    puts "北海道"
+  # elsif num % 3  then
+  #   puts "北海道"
+  # elsif num % 5 then
+  #   puts "東京"
+   else
+     puts num
+  end
+end
